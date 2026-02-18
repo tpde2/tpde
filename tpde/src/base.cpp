24cc65b3371ff400b59873ff9afac02e7f4a0163
Marc Auberer <marc.auberer@sap.com>
2026-02-18 16:07:19 +0000

    [TPDE] Throw runtime_error on fatal_error if eh is enabled

--- tpde/src/base.cpp
+++ tpde/src/base.cpp
@@ -4,12 +4,17 @@
 #include "tpde/base.hpp"
 
 #include <cstdlib>
+#include <stdexcept>
 
 namespace tpde {
 
 [[noreturn]] void fatal_error([[maybe_unused]] const char *msg) {
   TPDE_LOG_ERR("TPDE FATAL ERROR: {}", msg);
+#ifdef __cpp_exceptions
+  throw std::runtime_error(msg);
+#else
   abort();
+#endif
 }
 
 } // end namespace tpde
